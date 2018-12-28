a7ad1778e718e437f49569535f0603e63dbe5496
John Keller <jmkeller3@gmail.com>
2018-12-27 20:18:11 -0700

    added some sample content

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Sidebar from "./Sidebar";
+import Tim from "../img/Tim.jpg";
+import Zach from "../img/Zach.jpg";
 
 export default class About extends Component {
   render() {
@@ -7,6 +9,53 @@ export default class About extends Component {
       <main>
         <div className="main">
           <h2>About</h2>
+
+          <div id="abt-church">
+            <h3>Christ Fellowship</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Inventore accusantium et debitis hic veniam officia fugit ipsa
+              eius quos, ab voluptate voluptatibus sunt aliquid necessitatibus
+              facilis dolorum quas sed libero numquam tempore quibusdam error
+              sit? Incidunt dicta laudantium aspernatur quos!
+            </p>
+          </div>
+          <div id="abt-elders">
+            <h3>The Leaders</h3>
+            <div className="elder">
+              <h4>Timothy O'Day</h4>
+              <img src={Tim} alt="A portrait of Timothy O'Day" />
+
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Inventore accusantium et debitis hic veniam officia fugit ipsa
+                eius quos, ab voluptate voluptatibus sunt aliquid necessitatibus
+                facilis dolorum quas sed libero numquam tempore quibusdam error
+                sit? Incidunt dicta laudantium aspernatur quos!
+              </p>
+            </div>
+            <div className="elder">
+              <h4>Zach Thompson</h4>
+              <img src={Zach} alt="A portrait of Zach Thompson" />
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Inventore accusantium et debitis hic veniam officia fugit ipsa
+                eius quos, ab voluptate voluptatibus sunt aliquid necessitatibus
+                facilis dolorum quas sed libero numquam tempore quibusdam error
+                sit? Incidunt dicta laudantium aspernatur quos!
+              </p>
+            </div>
+          </div>
+          <div id="abt-location">
+            <h3>Lehi, UT</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Inventore accusantium et debitis hic veniam officia fugit ipsa
+              eius quos, ab voluptate voluptatibus sunt aliquid necessitatibus
+              facilis dolorum quas sed libero numquam tempore quibusdam error
+              sit? Incidunt dicta laudantium aspernatur quos!
+            </p>
+          </div>
         </div>
         <div className="sidebar">
           <Sidebar
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,17 @@ export default class Contact extends Component {
       <main>
         <div className="main">
           <h2>Contact</h2>
+          <div id="connect">
+            <p>Connect with us at the following places</p>
+            <span>Email | Facebook | Twitter</span>
+          </div>
+          <div id="gathering">
+            <p>Come visit with us</p>
+            <p>Every Sunday: 10:30 AM</p>
+          </div>
+          <div id="address">
+            <p>123 N Elm Dr. Lehi UT, 84043</p>
+          </div>
         </div>
         <div className="sidebar">
           <Sidebar links={["Contact Church", "Vist", "Address"]} />
