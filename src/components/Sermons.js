cebe683119e04568e01ff5b6e9b2fa6db7814dde
John Keller <jmkeller3@gmail.com>
2019-01-19 14:59:48 -0700

    added firebase file

--- src/components/Sermons.js
+++ src/components/Sermons.js
@@ -6,19 +6,17 @@ export default class Sermons extends Component {
   render() {
     return (
       <main id="sermons">
-        <div className="main">
-          <div className="sermon-browser">
-            <iframe
-              tabIndex="-1"
-              width="1"
-              height="540"
-              src="https://embed.sermonaudio.com/browser/broadcaster/gbccapecoral/speaker/tom%20ascol/?sort=newest&page_size=10&style=compact&footer=false&filters=false"
-              style={{ minWidth: "100%", maxWidth: "100%" }}
-              allow="autoplay"
-              frameBorder="0"
-              scrolling="no"
-            />
-          </div>
+        <div className="sermon-browser">
+          <iframe
+            tabIndex="-1"
+            width="1"
+            height="540"
+            src="https://embed.sermonaudio.com/browser/broadcaster/gbccapecoral/speaker/tom%20ascol/?sort=newest&page_size=10&style=compact&footer=false&filters=false"
+            style={{ minWidth: "100%", maxWidth: "100%" }}
+            allow="autoplay"
+            frameBorder="0"
+            scrolling="no"
+          />
         </div>
       </main>
     );
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyDpAknuvkiNqhQQXDrxF0o27B9FUySE5AM",
+  authDomain: "christ-fellowship-41797.firebaseapp.com",
+  databaseURL: "https://christ-fellowship-41797.firebaseio.com",
+  projectId: "christ-fellowship-41797",
+  storageBucket: "christ-fellowship-41797.appspot.com",
+  messagingSenderId: "431166887910"
+};
+firebase.initializeApp(config);
+export default firebase;
