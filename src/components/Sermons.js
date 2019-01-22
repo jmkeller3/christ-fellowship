0ca22bf4b98df98059071e09f24d613562de0972
John Keller <jmkeller3@gmail.com>
2019-01-21 17:06:22 -0700

    added audio files

--- src/components/Sermons.js
+++ src/components/Sermons.js
@@ -2,21 +2,17 @@ import React, { Component } from "react";
 
 import "./Sermons.css";
 
+import sermon from "../sermons/Test.m4a";
+
+const audio1 = new Audio(sermon);
+
 export default class Sermons extends Component {
   render() {
+    console.log(audio1);
     return (
       <main id="sermons">
         <div className="sermon-browser">
-          <iframe
-            tabIndex="-1"
-            width="1"
-            height="540"
-            src="https://embed.sermonaudio.com/browser/broadcaster/gbccapecoral/speaker/tom%20ascol/?sort=newest&page_size=10&style=compact&footer=false&filters=false"
-            style={{ minWidth: "100%", maxWidth: "100%" }}
-            allow="autoplay"
-            frameBorder="0"
-            scrolling="no"
-          />
+          <audio controls src={sermon} />
         </div>
       </main>
     );
