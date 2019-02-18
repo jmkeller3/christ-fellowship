347b1adbf1ec0619cf91509275db2c2a31155647
John Keller <jmkeller3@gmail.com>
2019-02-17 23:00:25 -0700

    Updates

---
+++ precache-manifest.10e6e74d911d98d80d50cd1dfb54c9fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "871437948200eb81770a08e8ff1221ad",
+    "url": "/static/media/Zach.87143794.jpeg"
+  },
+  {
+    "revision": "87eb56bd2a8fca9604912f77affafc5f",
+    "url": "/static/media/Tim.87eb56bd.jpg"
+  },
+  {
+    "revision": "140a3e57b264121495b89b645fbf5a8c",
+    "url": "/static/media/Prayer.140a3e57.jpg"
+  },
+  {
+    "revision": "12bde6cedbb3703f7f5ccb02a115284d",
+    "url": "/static/media/Logo.12bde6ce.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e7e29ae8f5fd5f8a2733",
+    "url": "/static/js/main.e7e29ae8.chunk.js"
+  },
+  {
+    "revision": "5459aa4e879c36449d76",
+    "url": "/static/js/2.5459aa4e.chunk.js"
+  },
+  {
+    "revision": "e7e29ae8f5fd5f8a2733",
+    "url": "/static/css/main.e42fc72a.chunk.css"
+  },
+  {
+    "revision": "5459aa4e879c36449d76",
+    "url": "/static/css/2.e0135f5b.chunk.css"
+  },
+  {
+    "revision": "19099dc10cb5bdf42f7b030ccc581572",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/christ-fellowship/precache-manifest.3bd2b22cc6cd5e461ad6825ca4c7bb5d.js"
+  "/precache-manifest.10e6e74d911d98d80d50cd1dfb54c9fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/christ-fellowship/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
