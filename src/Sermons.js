9e000bc5603f72509c319eefe6a26854561787e5
John Keller <jmkeller3@gmail.com>
2019-02-20 15:02:08 -0700

    followed basic tutorial

---
+++ src/Sermons.js
@@ -0,0 +1,33 @@
+
+import db from './firebase';
+
+const sermonList = document.querySelector('#sermon-list')
+
+function renderSermons(doc) {
+    let li = document.createElement('li');
+    let title = document.createElement('span');
+    let speaker = document.createElement('span');
+    let date = document.createElement('span');
+    let text = document.createElement('span');
+
+
+    li.setAttribute('data-id', doc.id);
+    title.textContent = doc.data.title;
+    speaker.textContent = doc.data.speaker;
+    date.textContent = doc.data.date;
+    text.textContent = doc.data.text;
+
+    li.appendChild(title);
+    li.appendChild(speaker);
+    li.appendChild(date);
+    li.appendChild(text);
+
+sermonList.appendChild(li)
+}
+
+db.collection('sermons').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data);
+        renderSermons(doc)
+    })
+})
