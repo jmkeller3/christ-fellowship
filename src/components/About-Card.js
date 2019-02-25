4fb7a8b4cb689124b651e8412fcd2382cf07ea1a
John Keller <jmkeller3@gmail.com>
2019-02-25 10:11:24 -0700

    fixed typo in about-card

--- src/components/About-Card.js
+++ src/components/About-Card.js
@@ -111,7 +111,7 @@ export class AboutCard extends React.Component {
                   Christ Fellowship
                 </Typography>
                 <Typography className={classes.padding} component="p">
-                  Christ Fellowship is a recently established church is Lehi,
+                  Christ Fellowship is a recently established church in Lehi,
                   UT. Our desire is to live in light of the love of God
                   displayed for us in the gospel of Jesus Christ. We say with
                   the apostle Paul:
